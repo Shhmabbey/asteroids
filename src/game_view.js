99255285f444cfb942e1a6c351fa120a0d68e5fc
shhmabbey <abhernndz@gmail.com>
2022-09-30 19:16:01 -0700

    Add key bindings

--- src/game_view.js
+++ src/game_view.js
@@ -3,33 +3,34 @@
 // Installs key listeners to move the ship and fire bullets.
 // Installs a timer to call Game.prototype.step.
 
-MOVES = {
-  w: [0, -1],
-  a: [-1, 0],
-  s: [0, 1],
-  d: [1, 0]
-};
+
 
 class GameView {
   constructor(game, ctx) {
     this.ctx = ctx;
     this.game = game;
-    // this.ship = this.game.addShip();
+    this.ship = this.game.addShip();
+    this.moves = {
+      w: [0, -1],
+      a: [-1, 0],
+      s: [0, 1],
+      d: [1, 0]
+    };
   }
 
-  // bindKeyHandlers() {
-  //   const ship = this.ship;
+  bindKeyHandlers() {
+    const ship = this.ship;
 
-  //   Object.keys(GameView.MOVES).forEach((k) => {
-  //     const move = GameView.MOVES[k];
-  //     key(k, function () { ship.power(move); });
-  //   });
+    Object.keys(this.moves).forEach((k) => {
+      const move = this.moves[k];
+      key(k, function () { ship.power(move); });
+    });
 
-  //   key("space", () => { ship.fireBullet(); });
-  // };
+    key("space", () => { ship.fireBullet(); });
+  };
 
   start() {
-    // this.bindKeyHandlers();
+    this.bindKeyHandlers();
     this.lastTime = 0;
     // start the animation // initial animation call
     requestAnimationFrame(this.animate.bind(this));
